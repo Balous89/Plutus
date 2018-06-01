6502aa081aece60b8d2907b1dec28329bfdebfc7
Balous89 <balut89@gmail.com>
2018-06-01 00:32:16 -0700

    conftest setup for create two routs for precreated user in db

--- Plutus/tests/conftest.py
+++ Plutus/tests/conftest.py
@@ -1,6 +1,11 @@
 
 import pytest
 from django.db import connection
+import datetime
+
+
+today = str(datetime.date.today())
+
 
 
 @pytest.fixture(scope='session')
@@ -8,8 +13,16 @@ def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         cur = connection.cursor()
         cur.execute("UPDATE django_site SET domain='127.0.0.1:8000' WHERE id=2;")
-        cur.execute("INSERT INTO `user_login_register_app_user`(`id`,`password`,`last_login`,`email`,`username`,`active`,`staff`,`admin`) VALUES (99,'bumerang','2018-05-24 19:14:47.149315','testuser99@gmail.com','testuser99','True','False','False');")
-        cur.execute("INSERT INTO `user_login_register_app_profile`(`id`,`email_confirmed`,`user_cretion_date`,`user_id`,`user_pln_per_km`) VALUES (99,1,'2018-05-24 00:00:00',99,2);")
+        cur.execute("INSERT INTO `user_login_register_app_user`(`id`,`password`,`last_login`,`email`,`username`,`active`,`staff`,`admin`) \
+        												VALUES (99,'bumerang','2018-05-24 19:14:47.149315','testuser99@gmail.com','testuser99','True','False','False');")
+        cur.execute("INSERT INTO `user_login_register_app_profile`(`id`,`email_confirmed`,`user_cretion_date`,`user_id`,`user_pln_per_km`) \
+        													VALUES (99,1,'2018-05-24 00:00:00',99,2);")
+        cur.execute("INSERT INTO `scribe_transitroutemodel`(`id`,`user_instance_id`,`origin_street`,`origin_city`,`origin_district`,`destination_street`,\
+        	`destination_city`,`destination_district`,`paycheck_for_route`,`distance_in_km`,`transit_date`) \
+        	VALUES (1,99,'Walecznych 59','Kłodzko','Dolnośląskie','Petuniowa 9','Wrocław','Dolnośląskie',176.8,88.4,'2018-05-01');")
+        cur.execute("INSERT INTO `scribe_transitroutemodel`(`id`,`user_instance_id`,`origin_street`,`origin_city`,`origin_district`,`destination_street`,\
+        	`destination_city`,`destination_district`,`paycheck_for_route`,`distance_in_km`,`transit_date`) \
+        	VALUES (2,99,'Walecznych 59','Kłodzko','Dolnośląskie','Wiejska 4','Warszawa','Mazowieckie',900,450," + today + ");")
 
         #cur.execute("INSERT INTO `user_login_register_app_user`(`id`,`password`,`last_login`,`email`,`username`,`active`,`staff`,`admin`) VALUES (99,'bumerang','2018-05-24 19:14:47.149315','testuser@gmail.com','testuser','True','False','False');")
         #cur.execute("INSERT INTO `scribe_transitroutemodel`(`id`,`user_instance_id`,`origin_street`,`origin_city`,`origin_district`,`destination_street`,`destination_city`,`destination_district`,`paycheck_for_route`,`distance_in_km`,`transit_date`) VALUES (1,99,'Testowa 1','Testowo Wielkie','Dolnośląskie','Testowa 99','Testowo Wielkie','Dolnośląskie','200.0','100.0','2018-05-28');")
--- Plutus/tests/dynamic.py
+++ Plutus/tests/dynamic.py
@@ -1,6 +1,5 @@
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
+import datetime
 
 
+print(datetime.date.today())
 
-print(urlsafe_base64_encode(force_bytes(-1)).decode())
